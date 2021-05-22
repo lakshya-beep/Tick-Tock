b2b03808ced0684cc7036e1d7d13b8ec3adc890d
lakshya-beep <74532103+lakshya-beep@users.noreply.github.com>
2021-05-22 12:35:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var hr = hour();
+var min = minute();
+var sec = second();
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
